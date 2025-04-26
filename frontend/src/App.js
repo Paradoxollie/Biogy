0096e588c7521bbbb115b0eebca8b36d7bd71e86
pierremarienne <pierremarienne@gmail.com>
2025-04-26 19:08:39 +0000

    fix: correct app and index.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,10 @@
-// frontend/src/App.js
 import React from 'react';
 
 function App() {
   return (
     <div>
       <h1>Biogy</h1>
-    </div>
-  );
+    </div>);
 }
 
 export default App;
-
-// frontend/src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
