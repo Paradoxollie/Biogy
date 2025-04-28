920bd163d28424b4ac15eb312cea7e248bbb4bef
Paradoxollie <want.to@hotmail.fr>
2025-04-28 23:05:42 +0200

    Fix Redux store implementation error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import App from './App'
 import { AuthProvider } from './context/AuthContext';
 import './index.css'; // Import Tailwind CSS
@@ -16,15 +18,32 @@ window.addEventListener('unhandledrejection', (event) => {
   console.error('Unhandled promise rejection:', event.reason);
 });
 
+// Create Redux store
+const store = configureStore({
+  reducer: {
+    // Add your reducers here when needed
+    user: (state = {}, action) => {
+      switch (action.type) {
+        case 'USER_LOGOUT':
+          return {};
+        default:
+          return state;
+      }
+    }
+  }
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <ErrorBoundary>
-            <HashRouter>
-                <AuthProvider>
-                    <App />
-                </AuthProvider>
-            </HashRouter>
+            <Provider store={store}>
+                <HashRouter>
+                    <AuthProvider>
+                        <App />
+                    </AuthProvider>
+                </HashRouter>
+            </Provider>
         </ErrorBoundary>
     </React.StrictMode>
 );
