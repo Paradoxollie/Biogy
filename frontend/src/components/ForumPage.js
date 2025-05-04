b671d5167e9df4225cf052fd7676b90fcc7d5878
Paradoxollie <want.to@hotmail.fr>
2025-05-04 16:23:08 +0200

    Correction de l'importation dans ForumPage.js et des redirections dans netlify.toml

--- frontend/src/components/ForumPage.js
+++ frontend/src/components/ForumPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import apiService from '../services/apiService';
+import apiService from '../services/api';
 import MaintenancePage from './MaintenancePage';
 
 function ForumPage() {
